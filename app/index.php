d85a963c17df14d9a965e9b8c5d6218d1e9c910e
Claus Hansen <claus@multimedion.dk>
2022-01-31 22:02:03 +0000

    mysqli install

--- app/index.php
+++ app/index.php
@@ -1,4 +1,11 @@
 <?php
+session_start();
+$mysqli = new mysqli("db","root","example","zbc_skolebase");
+
+if ($mysqli -> connect_errno) {
+  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+  exit();
+}
 $name = isset($_GET['name']) ? $_GET['name'] : '';
 echo '<h1>Hej '.$name.' , Velkommen til EUX!</h1>'; 
 ?>
--- dockerfile
+++ dockerfile
@@ -1,1 +1,3 @@
 FROM php:8.0.7-apache
+
+RUN  sudo apt-get install php-mysqlnd

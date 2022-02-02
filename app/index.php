8e341ddf602b723110fd0b560b334133b0b752f7
Claus Hansen <claus@multimedion.dk>
2022-02-02 05:25:42 +0000

    Fixed databasename

--- app/index.php
+++ app/index.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$mysqli = new mysqli("db","root","example","zbc_skolebase");
+$mysqli = new mysqli("db","root","example","zbc-skolebase");
 
 if ($mysqli -> connect_errno) {
   echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
